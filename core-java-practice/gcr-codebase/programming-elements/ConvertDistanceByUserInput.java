9f21013045c388062c8e318afd1a5f097b5ee901
DKR1688 <t.deepakrajput1688@gmail.com>
2025-12-18 12:12:15 +0530

    Distance km to miles using user input

---
+++ core-java-practice/gcr-codebase/programming-elements/ConvertDistanceByUserInput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class CovertDistanceByUserInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter distance in kilometers: ");
+        double km = scanner.nextDouble();
+
+        // here we are converting kilometers to miles
+        double miles = km * 0.621371;
+        System.out.println("Distance in miles: " + miles);
+        scanner.close();
+    }
+}
