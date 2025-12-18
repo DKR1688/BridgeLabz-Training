44be39add7f5d8ac5e438be05c6f962868487497
DKR1688 <t.deepakrajput1688@gmail.com>
2025-12-18 12:07:20 +0530

    Volume of earth in km and miles

---
+++ core-java-practice/gcr-codebase/programming-elements/VolumeOfEarth.java
@@ -0,0 +1,10 @@
+public class VolumeOfEarth {
+    public static void main(String[] args) {
+        int radiusOfEarthInKm = 6378;
+
+        // here we are calculating volume of earth in cubic kilometers
+        double VolumeOfEarth = (4* 3.14 * radiusOfEarthInKm * radiusOfEarthInKm * radiusOfEarthInKm)/3;
+
+        System.out.println("The volume of earth in cubic kilometers is "+ VolumeOfEarth+ " and in cubic miles is "+ VolumeOfEarth * 0.621371 * 0.621371 * 0.621371);
+    }
+}
