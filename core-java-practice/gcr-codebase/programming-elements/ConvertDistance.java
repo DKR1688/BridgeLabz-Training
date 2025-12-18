b0b15059dd9307a31142f0f511f583992ecacc03
DKR1688 <t.deepakrajput1688@gmail.com>
2025-12-18 12:28:12 +0530

    Distance converter

---
+++ core-java-practice/gcr-codebase/programming-elements/ConvertDistance.java
@@ -0,0 +1,9 @@
+public class ConvertDistance {
+    public static void main(String[] args) {
+        double distanceKm = 10.8;
+
+        // it is to converting kilometers to miles
+        double distanceMiles = distanceKm * 0.621371;
+        System.out.println(distanceKm+" kilometers is equal to "+distanceMiles+" miles.");
+    }
+}
